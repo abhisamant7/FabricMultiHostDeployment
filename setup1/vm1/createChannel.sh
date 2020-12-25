5bb7c91146a0eacd24dce64081b8846ba975d50e
abhisamant7 <abhisamanta29@gmail.com>
2020-12-25 17:39:49 +0530

    correction

--- setup1/vm1/createChannel.sh
+++ setup1/vm1/createChannel.sh
@@ -1,31 +1,31 @@
 export CORE_PEER_TLS_ENABLED=true
-export ORDERER_CA=${PWD}/../vm4/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export ORDERER_CA=${PWD}/../vm4/crypto-config/ordererOrganizations/dfarmorderer.com/orderers/dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
+export PEER0_Dfarmadmin_CA=${PWD}/crypto-config/peerOrganizations/dfarmadmin.com/peers/peer0.dfarmadmin.com/tls/ca.crt
 export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
 
-export CHANNEL_NAME=mychannel
+export CHANNEL_NAME=yngchannel
 
-setGlobalsForPeer0Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+setGlobalsForPeer0Dfarmadmin(){
+    export CORE_PEER_LOCALMSPID="DfarmadminMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_Dfarmadmin_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/dfarmadmin.com/users/Admin@dfarmadmin.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
 }
 
-setGlobalsForPeer1Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+setGlobalsForPeer1Dfarmadmin(){
+    export CORE_PEER_LOCALMSPID="DfarmadminMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_Dfarmadmin_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/dfarmadmin.com/users/Admin@dfarmadmin.com/msp
     export CORE_PEER_ADDRESS=localhost:8051
 }
 
 createChannel(){
     rm -rf ./channel-artifacts/*
-    setGlobalsForPeer0Org1
-    
+    setGlobalsForPeer0Dfarmadmin
+
     # Replace localhost with your orderer's vm IP address
     peer channel create -o localhost:7050 -c $CHANNEL_NAME \
-    --ordererTLSHostnameOverride orderer.example.com \
+    --ordererTLSHostnameOverride dfarmorderer.com \
     -f ./../../artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
     --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
 }
@@ -33,21 +33,21 @@ createChannel(){
 # createChannel
 
 joinChannel(){
-    setGlobalsForPeer0Org1
+    setGlobalsForPeer0Dfarmadmin
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
-    setGlobalsForPeer1Org1
+
+    setGlobalsForPeer1Dfarmadmin
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
+
 }
 
 # joinChannel
 
 updateAnchorPeers(){
-    setGlobalsForPeer0Org1
+    setGlobalsForPeer0Dfarmadmin
     # Replace localhost with your orderer's vm IP address
-    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./../../artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-    
+    peer channel update -o localhost:7050 --ordererTLSHostnameOverride dfarmorderer.com -c $CHANNEL_NAME -f ./../../artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+
 }
 
 # updateAnchorPeers
