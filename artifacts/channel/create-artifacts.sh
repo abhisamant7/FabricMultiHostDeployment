13caf4c863b9005c4d700a4169c3a91234d005af
abhisamant7 <abhisamanta29@gmail.com>
2020-12-25 11:26:17 +0530

    configtx file

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -2,7 +2,7 @@
 chmod -R 0755 ./crypto-config
 # Delete existing artifacts
 rm -rf ./crypto-config
-rm genesis.block mychannel.tx
+rm genesis.block yngchannel.tx
 rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
@@ -13,8 +13,8 @@ rm -rf ../../channel-artifacts/*
 # System channel
 SYS_CHANNEL="sys-channel"
 
-# channel name defaults to "mychannel"
-CHANNEL_NAME="mychannel"
+# channel name defaults to "Yngchannel"
+CHANNEL_NAME="yngchannel"
 
 echo $CHANNEL_NAME
 
@@ -23,13 +23,10 @@ configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outp
 
 
 # Generate channel configuration block
-configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+configtxgen -profile Yngchannel -configPath . -outputCreateChannelTx ./yngchannel.tx -channelID $CHANNEL_NAME
 
-echo "#######    Generating anchor peer update for Org1MSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+echo "#######    Generating anchor peer update for DfarmadminMSP  ##########"
+configtxgen -profile Yngchannel -configPath . -outputAnchorPeersUpdate ./DfarmadminMSPanchors.tx -channelID $CHANNEL_NAME -asOrg DfarmadminMSP
 
-echo "#######    Generating anchor peer update for Org2MSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
-
-echo "#######    Generating anchor peer update for Org3MSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+echo "#######    Generating anchor peer update for YngadminMSP  ##########"
+configtxgen -profile Yngchannel -configPath . -outputAnchorPeersUpdate ./YngadminMSPanchors.tx -channelID $CHANNEL_NAME -asOrg YngadminMSP
